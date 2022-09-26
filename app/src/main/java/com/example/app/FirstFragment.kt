60bf7f6a996c05a4a34f9b996895014f5a55e3e0
Br3Gott <dawesohl@gmail.com>
2022-09-26 15:48:05 +0200

    Proof of concept network communication

--- app/src/main/java/com/example/app/FirstFragment.kt
+++ app/src/main/java/com/example/app/FirstFragment.kt
@@ -1,12 +1,20 @@
 package com.example.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.app.databinding.FragmentFirstBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -34,6 +42,57 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        var selected = true
+
+        binding.buttonSend.setOnClickListener {
+
+            val policy = ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            //val temp = URL("https://1.1.1.1/").readText()
+
+            //val temp = URL("http://10.255.146.43:5000").readText()
+
+            var reqParam = "{ \"1\": [[1000,0]]}"
+
+            if (selected) {
+                reqParam = "{ \"1\": [[1000,90]]}"
+                selected = false
+            } else {
+                selected = true
+            }
+
+            val mURL = URL("http://192.168.137.168:5000/motion")
+
+            with(mURL.openConnection() as HttpURLConnection) {
+                // optional default is GET
+                requestMethod = "POST"
+                setRequestProperty("Content-Type", "application/json")
+                setRequestProperty("Accept", "application/json")
+
+                val wr = OutputStreamWriter(getOutputStream());
+                wr.write(reqParam);
+                wr.flush();
+
+                println("URL : $url")
+                println("Response Code : $responseCode")
+
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuffer()
+
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    println("Response : $response")
+                }
+            }
+
+            binding.textviewFirst.text = "done"
         }
     }
 
