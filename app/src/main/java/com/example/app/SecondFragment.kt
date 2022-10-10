b727cabcb8e9362d0f036b18bc7cff0fa88249a8
Phillipdaw <phillip.dawkins@hotmail.com>
2022-10-10 16:05:44 +0200

    New test

--- app/src/main/java/com/example/app/SecondFragment.kt
+++ app/src/main/java/com/example/app/SecondFragment.kt
@@ -2,16 +2,18 @@ package com.example.app
 
 import android.content.ClipData
 import android.content.ClipDescription
+import android.graphics.Color
 import android.os.Bundle
 import android.view.DragEvent
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.app.databinding.FragmentSecondBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -39,57 +41,73 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.llRight.setOnDragListener(dragListener)
         binding.llBottom.setOnDragListener(dragListener)
         binding.imageView.setOnLongClickListener {
-            val clipText = "Hello"
+            val clipText = "Does this work?"
             val item = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item)
 
             val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+            it.startDragAndDrop(data, dragShadowBuilder, null, 0)
 
-            it.visibility = View.INVISIBLE
             true
         }
-        //binding.buttonSecond.setOnClickListener {
-           // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        //}
     }
 
     val dragListener = View.OnDragListener { view, event ->
     when(event.action){
         DragEvent.ACTION_DRAG_STARTED -> {
-            event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            if (event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                (view as? ImageView)?.setColorFilter(Color.BLUE)
+                view.invalidate()
+                true
+                }
+            else {
+                false
+            }
+
         }
         DragEvent.ACTION_DRAG_ENTERED -> {
+            binding.llBottom.alpha = 0.3f
             view.invalidate()
             true
         }
-        DragEvent.ACTION_DRAG_LOCATION -> true
+        DragEvent.ACTION_DRAG_LOCATION ->
+            true
         DragEvent.ACTION_DRAG_EXITED -> {
+            binding.llBottom.alpha = 1.0f
+            view.visibility = View.VISIBLE
             view.invalidate()
             true
         }
         DragEvent.ACTION_DROP -> {
-            val item = event.clipData.getItemAt(0)
+            binding.llBottom.alpha = 1.0f
+            val item : ClipData.Item = event.clipData.getItemAt(0)
             val dragData = item.text
-            //val toast = Toast.makeText(this@SecondFragment, dragData, Toast.LENGTH_SHORT)
-            //toast.show()
 
+
+            view.visibility = View.VISIBLE;
             view.invalidate()
 
-            val v = event.localState as View
-            val owner = v.parent as ViewGroup
-            owner.removeView(v)
-            val destination = view as LinearLayout
-            destination.addView(v)
-            v.visibility = View.VISIBLE
+            val parent = view.parent as LinearLayout
+
+            parent.removeView(view)
+
+            val dropArea = view as LinearLayout
+            dropArea.addView(view)
             true
         }
         DragEvent.ACTION_DRAG_ENDED -> {
+
+            view.visibility = View.VISIBLE;
             view.invalidate()
+            when(event.result){
+                true ->
+                    Toast.makeText(activity, "The drop was handled.", Toast.LENGTH_LONG)
+                else ->
+                    Toast.makeText(activity, "The drop didn't work.", Toast.LENGTH_LONG)
+            }.show()
             true
         }
         else -> false
