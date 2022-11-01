ebfcf4971daa55e3f517eba9b5794fee9605866d
Tax-Evad3r <alexc2898@hotmail.com>
2022-11-01 10:47:29 +0100

    For-loop to create buttons + dotted button

--- app/src/main/java/com/example/app/SecondFragment.kt
+++ app/src/main/java/com/example/app/SecondFragment.kt
@@ -1,5 +1,7 @@
 package com.example.app
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.content.ClipData
 import android.content.ClipDescription
 import android.os.Bundle
@@ -9,9 +11,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.animation.doOnEnd
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import android.widget.RelativeLayout
+import androidx.core.view.children
+import androidx.core.view.get
 import com.example.app.databinding.FragmentSecondBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,6 +27,11 @@ import com.example.app.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+
+    private lateinit var flipLeftIn:AnimatorSet
+    private lateinit var flipLeftOut:AnimatorSet
+    private lateinit var flipRightIn:AnimatorSet
+    private lateinit var flipRightOut:AnimatorSet
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,17 +42,52 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
-    ): View? {
+    ): View {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.llRight.setOnDragListener(dragListener)
+        val scale:Float = this.requireContext().resources.displayMetrics.density
+        binding.scRightMotions.cameraDistance = 8000 * scale
+        binding.scRightSounds.cameraDistance = 8000 * scale
+
+        flipLeftIn = AnimatorInflater.loadAnimator(activity, R.animator.flip_left_in) as AnimatorSet
+        flipLeftOut = AnimatorInflater.loadAnimator(activity, R.animator.flip_left_out) as AnimatorSet
+        flipRightIn = AnimatorInflater.loadAnimator(activity, R.animator.flip_right_in) as AnimatorSet
+        flipRightOut = AnimatorInflater.loadAnimator(activity, R.animator.flip_right_out) as AnimatorSet
+
+        binding.tabsRight.addOnTabSelectedListener(object : OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab?.position != 0)
+                {
+                    binding.scRightSounds.visibility = View.VISIBLE
+                    flipLeftIn.setTarget(binding.scRightSounds)
+                    flipLeftOut.setTarget(binding.scRightMotions)
+                    flipLeftIn.start()
+                    flipLeftOut.start()
+                    flipLeftIn.doOnEnd { binding.scRightMotions.visibility = View.GONE }
+                }
+                else
+                {
+                    binding.scRightMotions.visibility = View.VISIBLE
+                    flipRightIn.setTarget(binding.scRightMotions)
+                    flipRightOut.setTarget(binding.scRightSounds)
+                    flipRightIn.start()
+                    flipRightOut.start()
+                    flipRightIn.doOnEnd { binding.scRightSounds.visibility = View.GONE }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+
+        binding.llRightMotions.setOnDragListener(dragListener)
         binding.llBottom.setOnDragListener(dragListener)
         binding.motion.setOnLongClickListener {
             val clipText = "Hello"
@@ -53,79 +101,73 @@ class SecondFragment : Fragment() {
             it.visibility = View.INVISIBLE
             true
         }
-        binding.motion1.setOnLongClickListener {
-            val clipText = "Hello"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
 
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+        binding.buttonRun.setOnClickListener {
+            println("Current queue")
+            println(binding.llBottom.childCount)
+            for (i in 0 until binding.llBottom.childCount) {
+                println(binding.llBottom.getChildAt(i).contentDescription)
+            }
 
-            it.visibility = View.INVISIBLE
-            true
+            //FIX ME: Proof of concept until drag and drop is fully implemented,
+            //        data should also probably not be stored as static variable
+
+            //initialize two predefined motions
+            val motion1 = Data(mutableListOf(mutableListOf(1000, 0), mutableListOf(2000, 90)),mutableListOf(), mutableListOf(mutableListOf(1000, 0), mutableListOf(2000, 90)), mutableListOf(), mutableListOf(), mutableListOf())
+            val motion2 = Data(mutableListOf(mutableListOf(1000, 90), mutableListOf(2000, 180)), mutableListOf(), mutableListOf(), mutableListOf(mutableListOf(1000, 0), mutableListOf(2000, 90)), mutableListOf(), mutableListOf(mutableListOf(1000, 0), mutableListOf(2000, 180)))
+
+            //initialize empty initial response
+            var requestdata = Data(mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf())
+
+            //define motionCount as amount of motions on timeline
+            val motionCount = binding.llBottom.childCount
+
+            //if 1 or more motions on time line add 1:st motion to request
+            if (motionCount > 0) {
+                println("Adding motion1")
+                requestdata += motion1
+            }
+            //if 2 or more motions on time line add 2:nd motion to request
+            if (motionCount > 1) {
+                println("Adding motion2")
+                requestdata += motion2
+            }
+
+            //convert request to json
+            val jsonRequestdata = requestdata.toJson()
+            println("Sending json= $jsonRequestdata")
+
+            //send request
+            SendData().send(jsonRequestdata)
+
         }
-        binding.motion2.setOnLongClickListener {
-            val clipText = "Hello"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
 
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+        // for loop to create all buttons for movements
+        for (i in 0 until binding.llRightMotions.childCount) {
+            var x = binding.llRightMotions.getChildAt(i);
+            x.setOnLongClickListener {
+                val clipText = "Hello"
+                val item = ClipData.Item(clipText)
+                val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                val data = ClipData(clipText, mimeTypes, item)
 
-            it.visibility = View.INVISIBLE
-            true
+                val dragShadowBuilder = View.DragShadowBuilder(it)
+                it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+                it.visibility = View.INVISIBLE
+                true
+            }
         }
-        binding.motion3.setOnLongClickListener {
-            val clipText = "Hello"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
-
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
-        }
-        binding.motion4.setOnLongClickListener {
-            val clipText = "Hello"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
-
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
-        }
-        binding.motion5.setOnLongClickListener {
-            val clipText = "Hello"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
-
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
-        }
-        //binding.buttonSecond.setOnClickListener {
-           // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        //}
     }
 
-    val dragListener = View.OnDragListener { view, event ->
+    private val dragListener = View.OnDragListener { view, event ->
     when(event.action){
         DragEvent.ACTION_DRAG_STARTED -> {
             event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
         }
         DragEvent.ACTION_DRAG_ENTERED -> {
             val v = event.localState as View
-            v.setVisibility(View.VISIBLE);
+            v.visibility = View.VISIBLE;
             view.invalidate()
             true
         }
@@ -146,18 +188,42 @@ class SecondFragment : Fragment() {
             val owner = v.parent as ViewGroup
             owner.removeView(v)
             val destination = view as LinearLayout
-            destination.addView(v)
-            v.visibility = View.VISIBLE
+ /*           if (destination == binding.llBottom) {
+                val childcount = destination.childCount
+                val children = ArrayList<View>(childcount)
+
+                for (i in 1 until childcount) {
+                    if (destination.getChildAt(i) != null)
+                        children[i] = destination.getChildAt(i);
+                }
+
+                destination.removeAllViews()
+
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+
+                for (x in 1 until children.count()) {
+                    children[x] = event.localState as View
+                    destination.addView(children[x])
+                    children[x].visibility = View.VISIBLE
+                }
+            }
+            else {*/
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+            // }
             true
         }
         DragEvent.ACTION_DRAG_ENDED -> {
             val v = event.localState as View
-            v.setVisibility(View.VISIBLE);
+            v.visibility = View.VISIBLE;
             view.invalidate()
             true
         }
         else -> false
     }
+
+
 }
     override fun onDestroyView() {
         super.onDestroyView()
